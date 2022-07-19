0cffaa0ebb1bedf33304315fd19831e5eb1ce00e
Promise30 <promiseadeagbo@gmail.com>
2022-07-19 12:47:05 +0100

    Added ActiveLinkView and RecentLinkView to the file

--- I4G007469YZC/links/views.py
+++ I4G007469YZC/links/views.py
@@ -1,25 +1,56 @@
 from django.shortcuts import render
-from .models import Link
-from rest_framework import viewsets
+# from .models import Link
+from rest_framework import viewsets, status
 from .serizliers import LinkSerializer
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from django.utils import timezone
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models
+import datetime 
 # Create your views here.
 class PostListApi(ListAPIView):
-    queryset = Link.objects.filter(active=True)
+    queryset = models.Link.objects.filter(active=True)
     serializer_class = LinkSerializer
 
 class PostCreateApi(CreateAPIView):
-    queryset = Link.objects.filter(active=True)
+    queryset = models.Link.objects.filter(active=True)
     serializer_class = LinkSerializer
 
 class PostDetailApi(RetrieveAPIView):
-    queryset = Link.objects.filter(active=True)
+    queryset = models.Link.objects.filter(active=True)
     serializer_class = LinkSerializer
 
 class PostUpdateApi(UpdateAPIView):
-    queryset = Link.objects.filter(active=True)
+    queryset = models.Link.objects.filter(active=True)
     serializer_class = LinkSerializer
 
 class PostDeleteApi(DestroyAPIView):
-    queryset = Link.objects.filter(active=True)
+    queryset = models.Link.objects.filter(active=True)
     serializer_class = LinkSerializer
+
+class ActiveLinkView(APIView):
+    """
+    Returns a list of all active (publicly accessible) links
+    """
+    def get(self, request):
+        """ 
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        qs = models.Link.public.all()
+        data = LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+    
+class RecentLinkView(APIView):
+    """
+    Returns a list of recently created active links
+    """
+    def get(self, request):
+        """ 
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        seven_days_ago = timezone.now() - datetime.timedelta(days=7)
+        qs = models.Link.public.filter(created_date__gte=seven_days_ago)
+        data = LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+    
